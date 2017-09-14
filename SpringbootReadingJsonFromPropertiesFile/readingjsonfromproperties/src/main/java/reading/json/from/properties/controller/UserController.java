6ca5cae1eb7cf71e59cab3a179a6038fc07d93dc
sabin <nibaserc@gmail.com>
2017-09-14 15:28:41 -0500

    Wrapper class added

--- SpringbootReadingJsonFromPropertiesFile/readingjsonfromproperties/src/main/java/reading/json/from/properties/controller/UserController.java
+++ SpringbootReadingJsonFromPropertiesFile/readingjsonfromproperties/src/main/java/reading/json/from/properties/controller/UserController.java
@@ -7,11 +7,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import reading.json.from.properties.dataSetup.MockDataLoader;
 import reading.json.from.properties.model.User;
+import reading.json.from.properties.response.UserResponse;
 
 @RestController
 public class UserController {
@@ -23,7 +28,7 @@ public class UserController {
 
 	/* List<User> */
 	@GetMapping(value = "/")
-	public Map<Integer, User> users() {
+	public ResponseEntity<UserResponse> users() {
 		List<User> users = mockDataLoader.getUsers();
 		Map<Integer, User> myMap = new HashMap<Integer, User>();
 		users.forEach(user -> {
@@ -34,8 +39,12 @@ public class UserController {
 				myMap.put(user.getId(), user);
 			}
 		});
+		UserResponse response = new UserResponse();
+		response.setUsers(myMap);
 
-		return myMap;
+		return new ResponseEntity<UserResponse>(response, HttpStatus.OK);
+
+		// return myMap;
 		/*
 		 * List<User> users2 = new ArrayList<>(); myMap.forEach((k, v) -> {
 		 * users2.add(v);
--- SpringbootReadingJsonFromPropertiesFile/readingjsonfromproperties/src/main/java/reading/json/from/properties/response/UserResponse.java
+++ SpringbootReadingJsonFromPropertiesFile/readingjsonfromproperties/src/main/java/reading/json/from/properties/response/UserResponse.java
@@ -1,6 +1,7 @@
 package reading.json.from.properties.response;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +9,13 @@ import reading.json.from.properties.model.User;
 
 public class UserResponse {
 
-	@Getter
-	@Setter
-	private List<User> users;
+    @Getter
+    @Setter
+  /*  @JsonIgnore*/
+	private Map<Integer,User> users;
+    
+    public UserResponse() {
+		users=new HashMap<>();
+	}
 }
+
